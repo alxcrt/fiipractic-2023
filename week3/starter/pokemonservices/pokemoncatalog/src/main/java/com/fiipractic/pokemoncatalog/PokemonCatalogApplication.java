19525fc142d9faed073096eece4d9a86e9c819ab
Alexandru Cretu <cretu.alexandru2000@gmail.com>
2023-03-18 13:30:46 +0200

    week3-starter

---
+++ week3/starter/pokemonservices/pokemoncatalog/src/main/java/com/fiipractic/pokemoncatalog/PokemonCatalogApplication.java
@@ -0,0 +1,11 @@
+package com.fiipractic.pokemoncatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokemonCatalogApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PokemonCatalogApplication.class,args);
+    }
+}
